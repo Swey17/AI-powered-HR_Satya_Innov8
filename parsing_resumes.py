a4df309f7cedd8058da2861f5715cdb4e9a675e5
Swey17 <swayampandey21060617@gmail.com>
2025-09-26 01:18:34 +0530

    parsing resumes updated

--- parsing_resumes.py
+++ parsing_resumes.py
@@ -7,20 +7,47 @@ load_dotenv()
 
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "system",
-            "content": ""
-        },
-        {
-            "role": "user",
-            "content": "",
-        }
-    ],
+with open("system_prompt/resume_parsing_and_skill_score.txt", "r") as file:
+    system_prompt = file.read()
 
-    # The language model which will generate the completion.
-    model="llama-3.3-70b-versatile"
-)
+clear_json = input("do you want to clear the existing json file? (y/n): ")
+if clear_json.lower() == 'y':
+    open("parsed_resumes_with_skill_score.json", "w").close() # clear the file
 
-print(chat_completion.choices[0].message.content)
+main_json = {}
+
+no_of_resumes = 30
+main_json_file = open("parsed_resumes_with_skill_score.json", "w", encoding="utf-8")
+
+def parse_resume(resume_text, system_prompt):
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": system_prompt
+            },
+            {
+                "role": "user",
+                "content": resume_text
+            }
+        ],
+
+        model="llama-3.3-70b-versatile"
+    )
+
+    return chat_completion.choices[0].message.content
+
+for i in range(no_of_resumes):
+    with open(f"Final_resumes_txt/Resume_of_ID_{i}.txt", "r", encoding="utf-8") as file:
+        resume_text = file.read()
+
+    parsed_resume = parse_resume(resume_text, system_prompt)
+    parsed_resume_json = {i: eval(parsed_resume)} # convert string to dictionary
+    main_json.update(parsed_resume_json)
+
+main_json_file.write(str(main_json) + "\n") # write each resume's json in new line
+main_json_file.close()
+
+
+    
